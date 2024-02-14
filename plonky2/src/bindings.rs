d3f1142309fee6a5e0bff9d645b6f79d74662599
Dumi Loghin <dumiloghin@gmail.com>
2024-02-14 11:00:45 +0800

    alternative data copy to GPU

--- plonky2/src/bindings.rs
+++ plonky2/src/bindings.rs
@@ -235,6 +235,15 @@ extern "C" {
     );
 }
 extern "C" {
+    pub fn fill_digests_buf_in_rounds_in_c_on_gpu_nouploads(
+        digests_buf_size: u64,
+        cap_buf_size: u64,
+        leaves_buf_size: u64,
+        leaf_size: u64,
+        cap_height: u64,
+    );
+}
+extern "C" {
     pub fn fill_digests_buf_linear_cpu(
         digests_buf_size: u64,
         cap_buf_size: u64,
@@ -279,4 +288,13 @@ extern "C" {
 }
 extern "C" {
     pub fn get_leaves_ptr() -> *mut u64;
+}
+extern "C" {
+    pub fn get_digests_gpu_ptr() -> *mut u64;
+}
+extern "C" {
+    pub fn get_cap_gpu_ptr() -> *mut u64;
+}
+extern "C" {
+    pub fn get_leaves_gpu_ptr() -> *mut u64;
 }
