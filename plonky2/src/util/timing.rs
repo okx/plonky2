b4bcf39a5f9ada47263d85cf78419c02ee3ecbdc
Dumi Loghin <dumiloghin@gmail.com>
2024-04-12 13:52:49 +0800

    fix warning

--- plonky2/src/util/timing.rs
+++ plonky2/src/util/timing.rs
@@ -1,6 +1,7 @@
 use log::Level;
 #[cfg(feature = "timing")]
 use web_time::{Duration, Instant};
+#[cfg(not(feature = "timing"))]
 use log::log;
 
 /// The hierarchy of scopes, and the time consumed by each one. Useful for profiling.
