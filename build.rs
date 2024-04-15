0fc381fa8fe99294a7b310544f8d68d145b3a7ea
cliff0412 <cliff.yang@okg.com>
2024-04-15 16:13:38 +0800

    rm build.rs

--- build.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    // Run `git submodule update --init --recursive`
-    Command::new("git")
-        .args(&["submodule", "update", "--init", "--recursive"])
-        .status()
-        .expect("Failed to update submodules");
-}
