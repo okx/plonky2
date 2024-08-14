4a251d40f49624f9eea800ed1b01bf09ed17d68e
Dumitrel Loghin <dumitrel.loghin@okg.com>
2024-08-14 17:15:49 +0800

    port dev-dumi into this branch

---
+++ plonky2/tests/fibonacci_test.rs
@@ -0,0 +1,48 @@
+use plonky2::field::types::Field;
+use plonky2::iop::witness::{PartialWitness, WitnessWrite};
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+use plonky2::plonk::circuit_data::CircuitConfig;
+use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+
+#[cfg(feature = "cuda")]
+use crate::test_utils::init_cuda;
+#[cfg(feature = "cuda")]
+pub mod test_utils;
+
+#[test]
+fn test_fibonacci_proof() {
+    #[cfg(feature = "cuda")]
+    init_cuda();
+
+    const D: usize = 2;
+    type C = PoseidonGoldilocksConfig;
+    type F = <C as GenericConfig<D>>::F;
+
+    let config = CircuitConfig::standard_recursion_config();
+    let mut builder = CircuitBuilder::<F, D>::new(config);
+
+    // The arithmetic circuit.
+    let initial = builder.add_virtual_target();
+    let mut cur_target = initial;
+    for i in 2..101 {
+        let i_target = builder.constant(F::from_canonical_u32(i));
+        cur_target = builder.mul(cur_target, i_target);
+    }
+
+    // Public inputs are the initial value (provided below) and the result (which is generated).
+    builder.register_public_input(initial);
+    builder.register_public_input(cur_target);
+
+    let mut pw = PartialWitness::new();
+    pw.set_target(initial, F::ONE);
+
+    let data = builder.build::<C>();
+    let proof = data.prove(pw).unwrap();
+
+    println!(
+        "Factorial starting at {} is {}",
+        proof.public_inputs[0], proof.public_inputs[1]
+    );
+
+    let _ = data.verify(proof);
+}
